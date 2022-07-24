2dbb25c5e937f44e3296352fb378e605013d621c
jason <jason@hedmans.org>
2022-07-24 14:26:26 -0400

    filter staked nfts

--- hooks/useStakedNFTs.ts
+++ hooks/useStakedNFTs.ts
@@ -1,15 +1,19 @@
+import { useState, useEffect } from 'react';
+
 import moment from "moment";
 import { ethers } from "ethers";
 
-import { useAccount, useContractWrite, useContract, useSigner, useContractRead } from "wagmi";
+import { useAccount, useContractWrite, useContract, useSigner, useContractRead, useProvider } from "wagmi";
 import useOwnedNFTs from "./useLendOwnedNFTs/useOwnedNFTs";
 
 import doNFTABI from "../abis/doNFT.json";
 import marketABI from "../abis/market.json";
+import { Token } from "./types";
 
 const useStakedNFTs = (contractAddress: string) => {
 
     const { address } = useAccount();
+    const provider = useProvider();
     const { data: signer } = useSigner()
 
     const {
@@ -25,8 +29,30 @@ const useStakedNFTs = (contractAddress: string) => {
     const doNFTContract = useContract({
         addressOrName: contractAddress,
         contractInterface: doNFTABI,
+        signerOrProvider: provider
+    })
+
+    const { write: redeem } = useContractWrite({
+        addressOrName: contractAddress,
+        contractInterface: doNFTABI,
+        functionName: 'redeem',
         signerOrProvider: signer
     })
+
+    const [vNfts, setVNfts] = useState<Token[]>([]);
+
+    useEffect(() => {
+        const getVNfts = async () => {
+            const areVNFTs = await Promise.all(stakedNFTs.map(async nft => {
+                const isVNFT = await doNFTContract.isVNft(nft.tokenId);
+                return isVNFT;
+            }))
+            setVNfts(stakedNFTs.filter((_, i) => areVNFTs[i]));
+        }
+        if(stakedNFTs.length > 0 && doNFTContract) {
+            getVNfts();
+        }
+    }, [stakedNFTs, doNFTContract])
 
     const createSigma = async (
         tokenId: number,
@@ -49,15 +75,17 @@ const useStakedNFTs = (contractAddress: string) => {
     // todo: call redeem on Market contract
     const redeemVNFT = async (tokenId: number) => {
         const durationList = await doNFTContract.getDurationIdList(tokenId);
-        await doNFTContract.redeem(
-            tokenId,
-            durationList.map(duration => duration.toNumber())
-        )
+        await redeem({
+            args: [
+                tokenId,
+                durationList.map(duration => duration.toNumber())
+        ]
+        })
     }
 
     return {
         walletConnected: Boolean(address),
-        stakedNFTs,
+        stakedNFTs: vNfts,
         createSigma,
         redeemVNFT,
     };
