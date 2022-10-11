407db0e6b64f611bec559c4ca4cfe0ecc13a4171
jason <jason@hedmans.org>
2022-10-11 10:06:56 -0500

    add metadata and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,9 @@ export default function Document() {
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" />  
+            <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet" /> 
+            <title>Decentralease</title>
+            <meta name="description" content="NFT renting, leasing, BNPL, and free trials" />
         </Head>
         <body>
             <Main />
